e7c879a1c874a24332f49815be6bde1899e15ae4
Olena Oliianenko <felangelena@gmail.com>
2022-01-18 19:42:16 +0100

    feat: add reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let string = n.toString();
+    let array = string.split('');
+    let reverseArray = array.reverse();
+    let reverseString = reverseArray.join('');
+    let reverseNumber = parseInt(reverseString);
+    return reverseNumber;
+};
